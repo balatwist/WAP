a4156d80f63d4fcb84d35292f443dc9fa733e319
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-12-14 14:43:02 -0600

    u

--- LAB11/question2.js
+++ LAB11/question2.js
@@ -8,48 +8,47 @@ const server = http.createServer((request, response) => {
     if (url === '/' || url.toLowerCase() === '/home') {
         response.end("Welcome to my website");
     } else if (url.toLowerCase() === '/image') {
-        // Read the image file
+       
         const imagePath = path.join(__dirname, 'dog.jpeg');
         
-        // Check if the file exists
+       
         if (fs.existsSync(imagePath)) {
             const imageStream = fs.createReadStream(imagePath);
 
-            // Set the appropriate content type for a JPEG image
+            
             response.setHeader('Content-Type', 'image/jpeg');
 
-            // Handle errors during streaming
+          
             imageStream.on('error', (error) => {
                 console.error(error);
                 response.writeHead(500);
                 response.end("Internal Server Error");
             });
 
-            // Pipe the image stream to the response
+           
             imageStream.pipe(response);
         } else {
             response.writeHead(404);
             response.end("Error 404: Image not found");
         }
     } else if (url.toLowerCase() === '/pdf') {
-        // Read the PDF file
+       
         const pdfPath = path.join(__dirname, 'file.pdf');
 
-        // Check if the file exists
+     
         if (fs.existsSync(pdfPath)) {
             const pdfStream = fs.createReadStream(pdfPath);
 
-            // Set the appropriate content type for a PDF
             response.setHeader('Content-Type', 'application/pdf');
 
-            // Handle errors during streaming
+           
             pdfStream.on('error', (error) => {
                 console.error(error);
                 response.writeHead(500);
                 response.end("Internal Server Error");
             });
 
-            // Pipe the PDF stream to the response
+            
             pdfStream.pipe(response);
         } else {
             response.writeHead(404);
